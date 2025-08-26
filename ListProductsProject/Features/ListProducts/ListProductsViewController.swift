264ed90de7bcc0ed526977289bf8c57b2debd299
Mohanad-Ramadan <mohanadramadan.m1@gmail.com>
2025-08-26 19:50:09 +0300

    Build the initial setup for ListProductsViewController: - create a collectionView - setup the components delegates and dataSource - setup constraints

--- ListProductsProject/Features/ListProducts/ListProductsViewController.swift
+++ ListProductsProject/Features/ListProducts/ListProductsViewController.swift
@@ -8,24 +8,75 @@
 import UIKit
 
 class ListProductsViewController: UIViewController {
+    // MARK: Properties
+    private lazy var productsCollectionView : UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.backgroundColor = .systemGroupedBackground
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
     
-    
-
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
-        // Do any additional setup after loading the view.
+        setupView()
+        setupConstraints()
+        setupCollectionView()
+        setupCollectionViewLayout()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Setup Views
+    private func setupView() {
+        title = "Products:"
+        view.backgroundColor = .systemGroupedBackground
     }
-    */
+    
+    private func setupCollectionViewLayout() {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 10
+        layout.minimumInteritemSpacing = 0
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        productsCollectionView.setCollectionViewLayout(layout, animated: false)
+    }
+    
+    private func setupConstraints() {
+        view.addSubview(productsCollectionView)
+        
+        NSLayoutConstraint.activate([
+            productsCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            productsCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            productsCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            productsCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    private func setupCollectionView() {
+        productsCollectionView.dataSource = self
+        productsCollectionView.delegate = self
+        productsCollectionView.register(
+            UICollectionViewCell.self,
+            forCellWithReuseIdentifier: "cell"
+        )
+    }
+    
+}
 
+extension ListProductsViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    // MARK: - UICollectionViewDataSource
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .blue
+        return cell    }
+    
+    // MARK: - UICollectionViewDelegateFlowLayout
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.bounds.width - 20
+        return CGSize(width: width, height: 100)
+    }
+    
 }
